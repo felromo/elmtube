ae61ce31c960b97ca9b63b92f8fa25550f54b9fd
Tyrantz <tyrantwarzhip@gmail.com>
2016-12-23 00:17:03 -0600

    base structure

---
+++ src/Main.elm
@@ -0,0 +1,73 @@
+module Main exposing (..)
+
+import Html exposing (..)
+import Html.Events exposing (..)
+
+
+-- import Html.Attributes exposing (..)
+
+
+main =
+    Html.program
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
+
+
+
+-- MODEL
+
+
+type alias Model =
+    { input : String
+    , searchTerm : String
+    , searchReturn : List Video
+    , activeVideo : Video
+    }
+
+
+type alias Video =
+    { id : String
+    , name : String
+    }
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( Model "" "" [] { id = "", name = "" }, Cmd.none )
+
+
+
+-- UPDATE
+
+
+type Msg
+    = NoOp
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        NoOp ->
+            ( model, Cmd.none )
+
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
+
+
+-- VIEW
+
+
+view : Model -> Html.Html Msg
+view model =
+    div []
+        [ text "placeholder" ]
